c553774df97710902ccb45e069c2c4d055cc53e9
Dawid Chorazy <dawid.chorazy@microsoft.wsei.edu.pl>
2025-06-10 12:10:29 +0200

    commands.ts command fix

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -10,11 +10,10 @@ Cypress.Commands.add('searchFor', (term: string): void => {
 
 Cypress.Commands.add('addProductToCartByName', ({ name }: { name: string }): void => {
   cy.contains('.prdocutname', name, { timeout: 10000 })
-    .should('be.visible')
-    .parents('.thumbnail')
-    .find('.productcart')
-    .should('be.visible')
-    .click();
+  cy.get('.fa.fa-cart-plus.fa-fw')
+  .should('be.visible')
+  .first()
+  .click()
 });
 
 
